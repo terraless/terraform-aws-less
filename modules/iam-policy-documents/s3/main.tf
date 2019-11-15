2721e69a76bd4937e62563db5bc3e3fa569672a8
Yujun Zhang <yujunz@nvidia.com>
2019-11-15 13:59:43 +0800

    Fix IAM policy document for S3

--- modules/iam-policy-documents/s3/main.tf
+++ modules/iam-policy-documents/s3/main.tf
@@ -13,7 +13,7 @@ module "arns_all" {
   s3_buckets = ["*"]
 }
 
-data "aws_iam_policy_document" "s3_full_access" {
+data "aws_iam_policy_document" "base" {
   statement {
     actions = [
       "s3:ListAllMyBuckets",
@@ -22,6 +22,10 @@ data "aws_iam_policy_document" "s3_full_access" {
 
     resources = module.arns_all.s3_bucket_arns
   }
+}
+
+data "aws_iam_policy_document" "full_access" {
+  source_json = data.aws_iam_policy_document.base.json
 
   statement {
     actions = [
@@ -35,7 +39,50 @@ data "aws_iam_policy_document" "s3_full_access" {
   }
 }
 
-output "s3_bucket_full_access" {
+data "aws_iam_policy_document" "read_only" {
+  source_json = data.aws_iam_policy_document.base.json
+
+  statement {
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+    ]
+
+    resources = concat(
+      module.arns_this.s3_bucket_arns,
+      formatlist("%s/*", module.arns_this.s3_bucket_arns)
+    )
+  }
+}
+
+data "aws_iam_policy_document" "read_write" {
+  source_json = data.aws_iam_policy_document.base.json
+
+  statement {
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+      "s3:Put*",
+    ]
+
+    resources = concat(
+      module.arns_this.s3_bucket_arns,
+      formatlist("%s/*", module.arns_this.s3_bucket_arns)
+    )
+  }
+}
+
+output "full_access_json" {
   description = "S3 bucket full access IAM policy document"
-  value       = data.aws_iam_policy_document.s3_full_access.json
+  value       = data.aws_iam_policy_document.full_access.json
+}
+
+output "read_only_json" {
+  description = "S3 bucket read only IAM policy document"
+  value       = data.aws_iam_policy_document.read_only.json
+}
+
+output "read_write_json" {
+  description = "S3 bucket read write IAM policy document"
+  value       = data.aws_iam_policy_document.read_write.json
 }
